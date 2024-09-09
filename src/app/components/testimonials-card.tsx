16aaf36cd50acf154d9b03b285d15745e479bee2
souzannatha <souzanatha04@gmail.com>
2024-09-09 13:56:17 -0300

    fix: adjust testimonials components

--- src/app/components/testimonials-card.tsx
+++ src/app/components/testimonials-card.tsx
@@ -14,28 +14,33 @@ export function TestimonialsCards({
   imageSrc,
 }: TestimonialsCardsProps) {
   return (
-    <div className="relative flex items-center w-[760px] h-[530px]">
+    <div className="relative flex items-center w-[760px] h-[530px] mobile:w-[350px] mobile:h-[250px]">
       <div className="relative">
         <Image
           src="quotes.svg"
           width={80}
           height={80}
           alt="Citação"
-          className="absolute -top-9 -left-10"
+          className="absolute -top-9 -left-10 mobile:w-10 h-10 mobile:-top-2 mobile:-left-6"
         />
         <Image
           src={imageSrc}
           width={370}
           height={500}
           alt={`Imagem de ${name}`}
+          className="mobile:w-44 mobile:h-60"
         />
       </div>
-      <div className="w-[305px] h-[190px] ml-10">
-        <h2 className="font-bold text-4xl text-blueCustom-blue05">{name}</h2>
-        <h3 className="font-semibold text-xl text-blueCustom-blueBlack mb-2">
+      <div className="w-[305px] h-[190px] ml-10 mobile:w-36 mobile:h-40 mobile:ml-5">
+        <h2 className="font-bold text-4xl text-blueCustom-blue05 mobile:text-base">
+          {name}
+        </h2>
+        <h3 className="font-semibold text-xl text-blueCustom-blueBlack mb-2 mobile:text-xs">
           {description}
         </h3>
-        <p className="text-lg text-blueCustom-blue05">{testimonial}</p>
+        <p className="text-lg text-blueCustom-blue05 mobile:text-sm">
+          {testimonial}
+        </p>
       </div>
     </div>
   );
--- src/app/components/testimonials.tsx
+++ src/app/components/testimonials.tsx
@@ -2,16 +2,16 @@ import { TestimonialsCards } from "./testimonials-card";
 
 export function Testimonial() {
   return (
-    <section className="w-[1544px] h-[750px] mx-auto my-auto">
-      <div className="w-[380px] h-32 mb-20">
-        <p className="font-medium text-xl text-blueCustom-blue05">
+    <section className="w-[1544px] h-[750px] mx-auto my-auto mobile:ml-7">
+      <div className="w-[380px] h-32 mb-20 mobile:w-[250px] mobile:mb-8">
+        <p className="font-medium text-xl text-blueCustom-blue05 mobile:text-sm">
           Depoimentos
         </p>
-        <h2 className="font-bold text-[52px] text-blueCustom-blue01">
+        <h2 className="font-bold text-[52px] text-blueCustom-blue01 mobile:text-3xl">
           O que nossos clientes dizem
         </h2>
       </div>
-      <div className="flex justify-between">
+      <div className="flex justify-between mobile:flex-col">
         <TestimonialsCards
           name="Maria Silva"
           description="Aposentada - Londrina"
